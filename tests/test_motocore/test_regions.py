5bc701d2affa0086f0afed761f34984d561d5a8b
Brian Pandola <bpandola@hsdp.io>
2021-01-13 22:56:07 -0800

    Blacked

--- tests/test_motocore/test_regions.py
+++ tests/test_motocore/test_regions.py
@@ -1,4 +1,3 @@
-
 from botocore.session import get_session
 from moto.motocore.regions import EndpointResolver
 
@@ -7,26 +6,28 @@ from moto.motocore.regions import EndpointResolver
 
 def test_endpoint_deconstruction():
     session = get_session()
-    loader = session.get_component('data_loader')
-    endpoint_data = loader.load_data('endpoints')
+    loader = session.get_component("data_loader")
+    endpoint_data = loader.load_data("endpoints")
     deconstructor = EndpointResolver(endpoint_data)
-    result = deconstructor.deconstruct_endpoint('rds.us-west-2.amazonaws.com')
-    assert result['region'] == 'us-west-2'
-    assert result['partition'] == 'aws'
-    assert result['service'] == 'rds'
+    result = deconstructor.deconstruct_endpoint("rds.us-west-2.amazonaws.com")
+    assert result["region"] == "us-west-2"
+    assert result["partition"] == "aws"
+    assert result["service"] == "rds"
     # This tests {region}.{service} as well as service alias
-    result = deconstructor.deconstruct_endpoint('us-west-1.elasticmapreduce.amazonaws.com')
-    assert result['region'] == 'us-west-1'
-    assert result['partition'] == 'aws'
-    assert result['service'] == 'elasticmapreduce'
+    result = deconstructor.deconstruct_endpoint(
+        "us-west-1.elasticmapreduce.amazonaws.com"
+    )
+    assert result["region"] == "us-west-1"
+    assert result["partition"] == "aws"
+    assert result["service"] == "elasticmapreduce"
 
 
 def test_endpoint_without_region():
     session = get_session()
-    loader = session.get_component('data_loader')
-    endpoint_data = loader.load_data('endpoints')
+    loader = session.get_component("data_loader")
+    endpoint_data = loader.load_data("endpoints")
     deconstructor = EndpointResolver(endpoint_data)
-    result = deconstructor.deconstruct_endpoint('iam.amazonaws.com')
-    assert result['partition'] == 'aws'
-    assert result['service'] == 'iam'
-    assert result['region'] == 'aws-global'
+    result = deconstructor.deconstruct_endpoint("iam.amazonaws.com")
+    assert result["partition"] == "aws"
+    assert result["service"] == "iam"
+    assert result["region"] == "aws-global"
