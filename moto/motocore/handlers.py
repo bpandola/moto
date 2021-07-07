e69dc7541557e77f835bce2106a6bf828fcb1ffb
Brian Pandola <bpandola@gmail.com>
2021-07-06 21:50:08 -0700

    Add some elasticbeanstalk and redshift hooks to make things work
    Man, there was some bad code in these responses/models

--- moto/motocore/handlers.py
+++ moto/motocore/handlers.py
@@ -72,6 +72,11 @@ def fix_ec2_result_issues(result_dict, operation_model, **kwargs):
         value.subnet_id = value.id
 
 
+def fix_elasticbeanstalk_result_issues(result_dict, operation_model, **kwargs):
+    if operation_model.name == "CreateEnvironment":
+        return result_dict.pop("result")
+
+
 def fix_emr_request_issues(parsed_request, operation_model, **kwargs):
     if operation_model.name == "RunJobFlow":
         params = parsed_request["kwargs"]
@@ -137,9 +142,28 @@ def fix_polly_result_issues(result_dict, operation_model, **kwargs):
 
 def fix_redshift_request_issues(parsed_request, operation_model, context, **kwargs):
     params = parsed_request["kwargs"]
-    if operation_model.name == "CreateCluster":
+    if operation_model.name in [
+        "CreateCluster",
+        "CreateClusterSubnetGroup",
+        "CreateClusterSecurityGroup",
+        "CreateClusterParameterGroup",
+    ]:
+        # This is because the current responses passes this in instead of
+        # letting the backend figure it out...
         params["region_name"] = context["region"]
-    elif operation_model.name == "CreateTags":
+    if operation_model.name == "CreateCluster":
+        if "iam_roles" in params:
+            params["iam_roles_arn"] = params.pop("iam_roles")
+    if operation_model.name == "CreateClusterParameterGroup":
+        if "parameter_group_name" in params:
+            params["cluster_parameter_group_name"] = params.pop("parameter_group_name")
+        if "parameter_group_family" in params:
+            params["group_family"] = params.pop("parameter_group_family")
+    if operation_model.name == "DeleteCluster":
+        params["skip_final_snapshot"] = params.pop("skip_final_cluster_snapshot", False)
+    if operation_model.name == "DescribeClusterSubnetGroups":
+        params["subnet_identifier"] = params.pop("cluster_subnet_group_name", None)
+    if operation_model.name == "CreateTags":
         # Have to make this case insensitive because the current
         # redshift response method calls the backend with Pascal Case.
         from botocore.awsrequest import HeadersDict as CaseInsensitiveDict
@@ -188,6 +212,11 @@ CUSTOM_HANDLERS = [
     # ("before-result-serialization.ec2.DescribeAvailabilityZones", fix_ec2_result_issues),
     # ("before-result-serialization.ec2.RunInstances", fix_ec2_result_issues),
     ("before-result-serialization.ec2.*", fix_ec2_result_issues),
+    # ElasticBeanStalk
+    (
+        "before-result-serialization.elastic-beanstalk.*",
+        fix_elasticbeanstalk_result_issues,
+    ),
     # EMR
     ("request-after-parsing.emr.*", fix_emr_request_issues),
     ("before-result-serialization.emr.*", fix_emr_result_issues),
