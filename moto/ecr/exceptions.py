9cfa77753128a9a01e60a6a803aada4ca2b915fe
Brian Pandola <bpandola@gmail.com>
2025-03-10 16:35:05 -0700

    Convert ecr exceptions

--- moto/ecr/exceptions.py
+++ moto/ecr/exceptions.py
@@ -1,112 +1,98 @@
-from moto.core.exceptions import JsonRESTError
+from moto.core.exceptions import MotoServiceException
 
 
-class LifecyclePolicyNotFoundException(JsonRESTError):
-    code = 400
+class LifecyclePolicyNotFoundException(MotoServiceException):
+    code = "LifecyclePolicyNotFoundException"
+    message = (
+        "Lifecycle policy does not exist "
+        "for the repository with name '{repository_name}' "
+        "in the registry with id '{registry_id}'"
+    )
 
     def __init__(self, repository_name: str, registry_id: str):
-        super().__init__(
-            error_type="LifecyclePolicyNotFoundException",
-            message=(
-                "Lifecycle policy does not exist "
-                f"for the repository with name '{repository_name}' "
-                f"in the registry with id '{registry_id}'"
-            ),
-        )
+        super().__init__(repository_name=repository_name, registry_id=registry_id)
 
 
-class LimitExceededException(JsonRESTError):
-    code = 400
-
-    def __init__(self) -> None:
-        super().__init__(
-            error_type="LimitExceededException",
-            message=("The scan quota per image has been exceeded. Wait and try again."),
-        )
+class LimitExceededException(MotoServiceException):
+    code = "LimitExceededException"
+    message = "The scan quota per image has been exceeded. Wait and try again."
 
 
-class RegistryPolicyNotFoundException(JsonRESTError):
-    code = 400
+class RegistryPolicyNotFoundException(MotoServiceException):
+    code = "RegistryPolicyNotFoundException"
+    message = "Registry policy does not exist in the registry with id '{registry_id}'"
 
     def __init__(self, registry_id: str):
-        super().__init__(
-            error_type="RegistryPolicyNotFoundException",
-            message=(
-                f"Registry policy does not exist in the registry with id '{registry_id}'"
-            ),
-        )
+        super().__init__(registry_id=registry_id)
 
 
-class RepositoryAlreadyExistsException(JsonRESTError):
-    code = 400
-
-    def __init__(self, repository_name: str, registry_id: str):
-        super().__init__(
-            error_type="RepositoryAlreadyExistsException",
-            message=(
-                f"The repository with name '{repository_name}' already exists "
-                f"in the registry with id '{registry_id}'"
-            ),
-        )
-
-
-class RepositoryNotEmptyException(JsonRESTError):
-    code = 400
+class RepositoryAlreadyExistsException(MotoServiceException):
+    code = "RepositoryAlreadyExistsException"
+    message = (
+        "The repository with name '{repository_name}' already exists "
+        "in the registry with id '{registry_id}'"
+    )
 
     def __init__(self, repository_name: str, registry_id: str):
-        super().__init__(
-            error_type="RepositoryNotEmptyException",
-            message=(
-                f"The repository with name '{repository_name}' "
-                f"in registry with id '{registry_id}' "
-                "cannot be deleted because it still contains images"
-            ),
-        )
+        super().__init__(repository_name=repository_name, registry_id=registry_id)
 
 
-class RepositoryNotFoundException(JsonRESTError):
-    code = 400
+class RepositoryNotEmptyException(MotoServiceException):
+    code = "RepositoryNotEmptyException"
+    message = (
+        "The repository with name '{repository_name}' "
+        "in registry with id '{registry_id}' "
+        "cannot be deleted because it still contains images"
+    )
 
     def __init__(self, repository_name: str, registry_id: str):
-        super().__init__(
-            error_type="RepositoryNotFoundException",
-            message=(
-                f"The repository with name '{repository_name}' does not exist "
-                f"in the registry with id '{registry_id}'"
-            ),
-        )
+        super().__init__(repository_name=repository_name, registry_id=registry_id)
 
 
-class RepositoryPolicyNotFoundException(JsonRESTError):
-    code = 400
+class RepositoryNotFoundException(MotoServiceException):
+    code = "RepositoryNotFoundException"
+    message = (
+        "The repository with name '{repository_name}' does not exist "
+        "in the registry with id '{registry_id}'"
+    )
 
     def __init__(self, repository_name: str, registry_id: str):
-        super().__init__(
-            error_type="RepositoryPolicyNotFoundException",
-            message=(
-                "Repository policy does not exist "
-                f"for the repository with name '{repository_name}' "
-                f"in the registry with id '{registry_id}'"
-            ),
-        )
+        super().__init__(repository_name=repository_name, registry_id=registry_id)
 
 
-class ImageNotFoundException(JsonRESTError):
-    code = 400
+class RepositoryPolicyNotFoundException(MotoServiceException):
+    code = "RepositoryPolicyNotFoundException"
+    message = (
+        "Repository policy does not exist "
+        "for the repository with name '{repository_name}' "
+        "in the registry with id '{registry_id}'"
+    )
+
+    def __init__(self, repository_name: str, registry_id: str):
+        super().__init__(repository_name=repository_name, registry_id=registry_id)
+
+
+class ImageNotFoundException(MotoServiceException):
+    code = "ImageNotFoundException"
+    message = (
+        "The image with imageId {image_id} does not exist "
+        "within the repository with name '{repository_name}' "
+        "in the registry with id '{registry_id}'"
+    )
 
     def __init__(self, image_id: str, repository_name: str, registry_id: str):
         super().__init__(
-            error_type="ImageNotFoundException",
-            message=(
-                f"The image with imageId {image_id} does not exist "
-                f"within the repository with name '{repository_name}' "
-                f"in the registry with id '{registry_id}'"
-            ),
+            image_id=image_id, repository_name=repository_name, registry_id=registry_id
         )
 
 
-class ImageAlreadyExistsException(JsonRESTError):
-    code = 400
+class ImageAlreadyExistsException(MotoServiceException):
+    code = "ImageAlreadyExistsException"
+    message = (
+        "Image with digest '{digest}' and tag '{image_tag}' already exists "
+        "in the repository with name '{repository_name}' "
+        "in registry with id '{registry_id}'"
+    )
 
     def __init__(
         self,
@@ -116,38 +102,30 @@ class ImageAlreadyExistsException(JsonRESTError):
         image_tag: str,
     ):
         super().__init__(
-            error_type="ImageAlreadyExistsException",
-            message=(
-                f"Image with digest '{digest}' and tag '{image_tag}' already exists "
-                f"in the repository with name '{repository_name}' "
-                f"in registry with id '{registry_id}'"
-            ),
+            repository_name=repository_name,
+            registry_id=registry_id,
+            digest=digest,
+            image_tag=image_tag,
         )
 
 
-class InvalidParameterException(JsonRESTError):
-    code = 400
-
-    def __init__(self, message: str):
-        super().__init__(error_type="InvalidParameterException", message=message)
+class InvalidParameterException(MotoServiceException):
+    code = "InvalidParameterException"
 
 
-class ScanNotFoundException(JsonRESTError):
-    code = 400
+class ScanNotFoundException(MotoServiceException):
+    code = "ScanNotFoundException"
+    message = (
+        "Image scan does not exist for the image with '{image_id}' "
+        "in the repository with name '{repository_name}' "
+        "in the registry with id '{registry_id}'"
+    )
 
     def __init__(self, image_id: str, repository_name: str, registry_id: str):
         super().__init__(
-            error_type="ScanNotFoundException",
-            message=(
-                f"Image scan does not exist for the image with '{image_id}' "
-                f"in the repository with name '{repository_name}' "
-                f"in the registry with id '{registry_id}'"
-            ),
+            image_id=image_id, repository_name=repository_name, registry_id=registry_id
         )
 
 
-class ValidationException(JsonRESTError):
-    code = 400
-
-    def __init__(self, message: str):
-        super().__init__(error_type="ValidationException", message=message)
+class ValidationException(MotoServiceException):
+    code = "ValidationException"
