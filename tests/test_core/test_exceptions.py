c1e5531b26f2c103866ef16cb7b3be4d3251c172
Brian Pandola <bpandola@gmail.com>
2025-03-09 23:34:54 -0700

    Add exception test

--- tests/test_core/test_exceptions.py
+++ tests/test_core/test_exceptions.py
@@ -10,6 +10,13 @@ class TestMotoServiceException:
         exc = TestMotoServiceException.TestException()
         assert str(exc) == "ExceptionCode: default message"
 
-    def test_override_exception_message(self) -> None:
+    def test_formatted_exception_message(self) -> None:
+        class FormattedException(MotoServiceException):
+            message = "The {resource_type} resource {resource_id} was not found!"
+
+        exc = FormattedException(resource_type="DBCluster", resource_id="cluster-id")
+        assert exc.message == "The DBCluster resource cluster-id was not found!"
+
+    def test_overridden_exception_message(self) -> None:
         exc = TestMotoServiceException.TestException("Override message")
         assert str(exc) == "ExceptionCode: Override message"
