a5320d5bb68153f18f1cafc57aa94f7ddf6fdb90
Brian Pandola <bpandola@gmail.com>
2025-03-21 13:49:17 -0700

    initial commit

--- tests/test_core/test_protocols.py
+++ tests/test_core/test_protocols.py
@@ -1,11 +1,18 @@
 # mypy: ignore-errors
+import base64
 import copy
 import json
 import os
+from calendar import timegm
 from enum import Enum
+from urllib.parse import parse_qs, urlparse
 
 import pytest
+from botocore.awsrequest import HeadersDict
+from botocore.utils import parse_timestamp
+from dateutil.tz import tzutc
 
+from moto.core.parse import PROTOCOL_PARSERS
 from moto.core.model import OperationModel, ServiceModel
 from moto.core.serialize import SERIALIZERS
 
@@ -56,6 +63,73 @@ def _load_cases(full_path):
             case["description"] = description
             case["id"] = index
             yield test_data, case, protocol
+
+
+def _compliance_blob_parser(value):
+    # By default, Blobs are returned as bytes type because it's possible
+    # that the blob contains binary data that actually can't be decoded.
+    # For the compliance tests, we decode to a string for easier asserts.
+    return base64.b64decode(value).decode("utf-8")
+
+
+def _compliance_timestamp_parser(value):
+    datetime = parse_timestamp(value)
+    datetime = datetime.astimezone(tzutc())
+    epoch_time = int(timegm(datetime.timetuple()))
+    return epoch_time
+
+
+def _build_query_params(query_string, body, headers):
+    def mock_multidict(data):
+        params = parse_qs(data, keep_blank_values=True)
+        params = {k: v if len(v) > 1 else v[0] for k, v in params.items()}
+        return params
+
+    query_params = mock_multidict(query_string)
+    if headers.get("Content-Type", "").startswith("application/x-www-form-urlencoded"):
+        body_params = mock_multidict(body)
+        query_params.update(body_params)
+    return query_params
+
+
+def _create_request_dict(given, serialized):
+    method = serialized.get("method", given.get("http", {}).get("method", "POST"))
+    # We need the headers to be case-insensitive
+    headers = HeadersDict(serialized.get("headers", {}))
+    uri = serialized.get("uri", "/")
+    parsed_uri = urlparse(uri)
+    query_string = parsed_uri.query
+    url_path = parsed_uri.path
+    body = serialized["body"]
+    query_params = _build_query_params(query_string, serialized["body"], headers)
+    request_dict = {
+        "method": method,
+        "body": body,
+        "headers": headers,
+        "query_string": query_string,
+        "query_params": query_params,
+        "url_path": url_path,
+    }
+    return request_dict
+
+
+@pytest.mark.parametrize(
+    "json_description, case, protocol", _compliance_tests(TestType.INPUT)
+)
+def test_input_compliance(json_description: dict, case: dict, protocol: str):
+    service_description = copy.deepcopy(json_description)
+    operation_name = case.get("given").get("name", "OperationName")
+    service_description["operations"] = {operation_name: case["given"]}
+    model = ServiceModel(service_description)
+    protocol_parser = PROTOCOL_PARSERS[protocol]
+    parser = protocol_parser(
+        blob_parser=_compliance_blob_parser,
+        timestamp_parser=_compliance_timestamp_parser,
+    )
+    request_dict = _create_request_dict(case["given"], case["serialized"])
+    parsed = parser.parse(request_dict, model)
+    assert parsed["action"] == operation_name
+    assert parsed["kwargs"] == case.get("params", {})
 
 
 @pytest.mark.parametrize(
