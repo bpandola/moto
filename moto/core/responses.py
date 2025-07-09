b034732dc9890ab1cbbcfebbb815db584c57a969
Brian Pandola <bpandola@gmail.com>
2025-07-09 03:36:00 -0700

    fix formatting

--- moto/core/responses.py
+++ moto/core/responses.py
@@ -308,6 +308,7 @@ class BaseResponse(_TemplateEnvironmentMixin, ActionAuthenticatorMixin):
     def dispatch(cls, *args: Any, **kwargs: Any) -> Any:  # type: ignore[misc]
         from werkzeug.wrappers.request import Request
         from botocore.awsrequest import AWSPreparedRequest
+        
         assert isinstance(args[0], (AWSPreparedRequest, Request))    
         return cls()._dispatch(*args, **kwargs)
 
