478a7d84b6ab5e774b46d6e00c2a5a1ab47031a3
Brian Pandola <bpandola@gmail.com>
2025-08-31 20:23:34 -0700

    Fix server mode test

--- tests/test_emr/test_server.py
+++ tests/test_emr/test_server.py
@@ -8,7 +8,9 @@ Test the different server responses
 def test_describe_jobflows():
     backend = server.create_backend_app("emr")
     test_client = backend.test_client()
-
-    res = test_client.get("/?Action=DescribeJobFlows")
-
+    headers = {
+        "Content-Type": "application/x-amz-json-1.1",
+        "X-Amz-Target": "ElasticMapReduce.DescribeJobFlows",
+    }
+    res = test_client.post("/", headers=headers)
     assert b"JobFlows" in res.data
