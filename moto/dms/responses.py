c04b77389c6b5b2bb64785dce08464c4382642f0
Brian Pandola <bpandola@gmail.com>
2026-04-17 01:13:11 -0700

    dms: migrate to automated parameter parsing

--- moto/dms/responses.py
+++ moto/dms/responses.py
@@ -8,6 +8,7 @@ from .models import DatabaseMigrationServiceBackend, dms_backends
 class DatabaseMigrationServiceResponse(BaseResponse):
     def __init__(self) -> None:
         super().__init__(service_name="dms")
+        self.automated_parameter_parsing = True
 
     @property
     def dms_backend(self) -> DatabaseMigrationServiceBackend:
@@ -71,26 +72,29 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         )
 
     def create_replication_instance(self) -> str:
-        params = json.loads(self.body)
-        replication_instance_identifier = params.get("ReplicationInstanceIdentifier")
-        allocated_storage = params.get("AllocatedStorage")
-        replication_instance_class = params.get("ReplicationInstanceClass")
+        replication_instance_identifier = self._get_param(
+            "ReplicationInstanceIdentifier"
+        )
+        allocated_storage = self._get_param("AllocatedStorage")
+        replication_instance_class = self._get_param("ReplicationInstanceClass")
         vpc_security_group_ids = self._get_param("VpcSecurityGroupIds")
-        availability_zone = params.get("AvailabilityZone")
-        replication_subnet_group_identifier = params.get(
+        availability_zone = self._get_param("AvailabilityZone")
+        replication_subnet_group_identifier = self._get_param(
             "ReplicationSubnetGroupIdentifier"
         )
-        preferred_maintenance_window = params.get("PreferredMaintenanceWindow")
-        multi_az = params.get("MultiAZ")
-        engine_version = params.get("EngineVersion")
-        auto_minor_version_upgrade = params.get("AutoMinorVersionUpgrade")
-        tags = params.get("Tags")
-        kms_key_id = params.get("KmsKeyId")
-        publicly_accessible = params.get("PubliclyAccessible")
-        dns_name_servers = params.get("DnsNameServers")
-        resource_identifier = params.get("ResourceIdentifier")
-        network_type = params.get("NetworkType")
-        kerberos_authentication_settings = params.get("KerberosAuthenticationSettings")
+        preferred_maintenance_window = self._get_param("PreferredMaintenanceWindow")
+        multi_az = self._get_param("MultiAZ")
+        engine_version = self._get_param("EngineVersion")
+        auto_minor_version_upgrade = self._get_param("AutoMinorVersionUpgrade")
+        tags = self._get_param("Tags")
+        kms_key_id = self._get_param("KmsKeyId")
+        publicly_accessible = self._get_param("PubliclyAccessible")
+        dns_name_servers = self._get_param("DnsNameServers")
+        resource_identifier = self._get_param("ResourceIdentifier")
+        network_type = self._get_param("NetworkType")
+        kerberos_authentication_settings = self._get_param(
+            "KerberosAuthenticationSettings"
+        )
         replication_instance = self.dms_backend.create_replication_instance(
             replication_instance_identifier=replication_instance_identifier,
             allocated_storage=allocated_storage,
@@ -113,10 +117,9 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         return json.dumps({"ReplicationInstance": replication_instance.to_dict()})
 
     def describe_replication_instances(self) -> str:
-        data = json.loads(self.body)
-        filters = data.get("Filters", [])
-        max_records = data.get("MaxRecords")
-        marker = data.get("Marker")
+        filters = self._get_param("Filters", [])
+        max_records = self._get_param("MaxRecords")
+        marker = self._get_param("Marker")
 
         replication_instances = self.dms_backend.describe_replication_instances(
             filters=filters,
@@ -137,42 +140,41 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         return json.dumps({"ReplicationInstance": replication_instance.to_dict()})
 
     def create_endpoint(self) -> str:
-        params = json.loads(self.body)
-        endpoint_identifier = params.get("EndpointIdentifier")
-        endpoint_type = params.get("EndpointType")
-        engine_name = params.get("EngineName")
-        username = params.get("Username")
-        password = params.get("Password")
-        server_name = params.get("ServerName")
-        port = params.get("Port")
-        database_name = params.get("DatabaseName")
-        extra_connection_attributes = params.get("ExtraConnectionAttributes")
-        kms_key_id = params.get("KmsKeyId")
-        tags = params.get("Tags")
-        certificate_arn = params.get("CertificateArn")
-        ssl_mode = params.get("SslMode")
-        service_access_role_arn = params.get("ServiceAccessRoleArn")
-        external_table_definition = params.get("ExternalTableDefinition")
-        dynamo_db_settings = params.get("DynamoDbSettings")
-        s3_settings = params.get("S3Settings")
-        dms_transfer_settings = params.get("DmsTransferSettings")
-        mongo_db_settings = params.get("MongoDbSettings")
-        kinesis_settings = params.get("KinesisSettings")
-        kafka_settings = params.get("KafkaSettings")
-        elasticsearch_settings = params.get("ElasticsearchSettings")
-        neptune_settings = params.get("NeptuneSettings")
-        redshift_settings = params.get("RedshiftSettings")
-        postgre_sql_settings = params.get("PostgreSQLSettings")
-        my_sql_settings = params.get("MySQLSettings")
-        oracle_settings = params.get("OracleSettings")
-        sybase_settings = params.get("SybaseSettings")
-        microsoft_sql_server_settings = params.get("MicrosoftSQLServerSettings")
-        ibm_db2_settings = params.get("IBMDb2Settings")
-        resource_identifier = params.get("ResourceIdentifier")
-        doc_db_settings = params.get("DocDbSettings")
-        redis_settings = params.get("RedisSettings")
-        gcp_my_sql_settings = params.get("GcpMySQLSettings")
-        timestream_settings = params.get("TimestreamSettings")
+        endpoint_identifier = self._get_param("EndpointIdentifier")
+        endpoint_type = self._get_param("EndpointType")
+        engine_name = self._get_param("EngineName")
+        username = self._get_param("Username")
+        password = self._get_param("Password")
+        server_name = self._get_param("ServerName")
+        port = self._get_param("Port")
+        database_name = self._get_param("DatabaseName")
+        extra_connection_attributes = self._get_param("ExtraConnectionAttributes")
+        kms_key_id = self._get_param("KmsKeyId")
+        tags = self._get_param("Tags")
+        certificate_arn = self._get_param("CertificateArn")
+        ssl_mode = self._get_param("SslMode")
+        service_access_role_arn = self._get_param("ServiceAccessRoleArn")
+        external_table_definition = self._get_param("ExternalTableDefinition")
+        dynamo_db_settings = self._get_param("DynamoDbSettings")
+        s3_settings = self._get_param("S3Settings")
+        dms_transfer_settings = self._get_param("DmsTransferSettings")
+        mongo_db_settings = self._get_param("MongoDbSettings")
+        kinesis_settings = self._get_param("KinesisSettings")
+        kafka_settings = self._get_param("KafkaSettings")
+        elasticsearch_settings = self._get_param("ElasticsearchSettings")
+        neptune_settings = self._get_param("NeptuneSettings")
+        redshift_settings = self._get_param("RedshiftSettings")
+        postgre_sql_settings = self._get_param("PostgreSQLSettings")
+        my_sql_settings = self._get_param("MySQLSettings")
+        oracle_settings = self._get_param("OracleSettings")
+        sybase_settings = self._get_param("SybaseSettings")
+        microsoft_sql_server_settings = self._get_param("MicrosoftSQLServerSettings")
+        ibm_db2_settings = self._get_param("IBMDb2Settings")
+        resource_identifier = self._get_param("ResourceIdentifier")
+        doc_db_settings = self._get_param("DocDbSettings")
+        redis_settings = self._get_param("RedisSettings")
+        gcp_my_sql_settings = self._get_param("GcpMySQLSettings")
+        timestream_settings = self._get_param("TimestreamSettings")
         endpoint = self.dms_backend.create_endpoint(
             endpoint_identifier=endpoint_identifier,
             endpoint_type=endpoint_type,
@@ -216,10 +218,9 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         )
 
     def describe_endpoints(self) -> str:
-        params = json.loads(self.body)
-        filters = params.get("Filters", [])
-        max_records = params.get("MaxRecords")
-        marker = params.get("Marker")
+        filters = self._get_param("Filters", [])
+        max_records = self._get_param("MaxRecords")
+        marker = self._get_param("Marker")
         endpoints = self.dms_backend.describe_endpoints(
             filters=filters, max_records=max_records, marker=marker
         )
@@ -240,9 +241,8 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         return json.dumps({"Endpoint": endpoint.to_dict()})
 
     def list_tags_for_resource(self) -> str:
-        params = json.loads(self.body)
-        resource_arn = params.get("ResourceArn")
-        resource_arn_list = params.get("ResourceArnList")
+        resource_arn = self._get_param("ResourceArn")
+        resource_arn_list = self._get_param("ResourceArnList")
 
         if resource_arn and resource_arn_list:
             raise ValueError(
@@ -261,15 +261,14 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         return json.dumps({"TagList": tag_list})
 
     def create_replication_subnet_group(self) -> str:
-        params = json.loads(self.body)
-        replication_subnet_group_identifier = params.get(
+        replication_subnet_group_identifier = self._get_param(
             "ReplicationSubnetGroupIdentifier"
         )
-        replication_subnet_group_description = params.get(
+        replication_subnet_group_description = self._get_param(
             "ReplicationSubnetGroupDescription"
         )
-        subnet_ids = params.get("SubnetIds")
-        tags = params.get("Tags")
+        subnet_ids = self._get_param("SubnetIds")
+        tags = self._get_param("Tags")
         replication_subnet_group = self.dms_backend.create_replication_subnet_group(
             replication_subnet_group_identifier=replication_subnet_group_identifier,
             replication_subnet_group_description=replication_subnet_group_description,
@@ -281,10 +280,9 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         )
 
     def describe_replication_subnet_groups(self) -> str:
-        params = json.loads(self.body)
-        filters = params.get("Filters", [])
-        max_records = params.get("MaxRecords")
-        marker = params.get("Marker")
+        filters = self._get_param("Filters", [])
+        max_records = self._get_param("MaxRecords")
+        marker = self._get_param("Marker")
         replication_subnet_groups = self.dms_backend.describe_replication_subnet_groups(
             filters=filters, max_records=max_records, marker=marker
         )
@@ -321,10 +319,9 @@ class DatabaseMigrationServiceResponse(BaseResponse):
         return json.dumps({"Connection": connection.to_dict()})
 
     def describe_connections(self) -> str:
-        data = json.loads(self.body)
-        filters = data.get("Filters", [])
-        max_records = data.get("MaxRecords")
-        marker = data.get("Marker")
+        filters = self._get_param("Filters", [])
+        max_records = self._get_param("MaxRecords")
+        marker = self._get_param("Marker")
 
         connections = self.dms_backend.describe_connections(
             filters=filters, max_records=max_records, marker=marker
