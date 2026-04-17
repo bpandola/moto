d4f475aac0b4de5c62274800f4edbe3814eeb7da
Brian Pandola <bpandola@gmail.com>
2026-04-17 01:13:17 -0700

    meteringmarketplace: migrate to automated parameter parsing

--- moto/meteringmarketplace/responses.py
+++ moto/meteringmarketplace/responses.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from moto.core.responses import BaseResponse
 
@@ -6,12 +7,20 @@ from .models import MeteringMarketplaceBackend, meteringmarketplace_backends
 
 
 class MarketplaceMeteringResponse(BaseResponse):
+    def __init__(self) -> None:
+        super().__init__(service_name="meteringmarketplace")
+        self.automated_parameter_parsing = True
+
     @property
     def backend(self) -> MeteringMarketplaceBackend:
         return meteringmarketplace_backends[self.current_account][self.region]
 
     def batch_meter_usage(self) -> str:
-        usage_records = json.loads(self.body)["UsageRecords"]
-        product_code = json.loads(self.body)["ProductCode"]
+        params = self._get_params()
+        usage_records = params["UsageRecords"]
+        for record in usage_records:
+            if isinstance(record.get("Timestamp"), datetime):
+                record["Timestamp"] = record["Timestamp"].isoformat()
+        product_code = params["ProductCode"]
         results = self.backend.batch_meter_usage(product_code, usage_records)
         return json.dumps({"Results": results, "UnprocessedRecords": []})
