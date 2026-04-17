5a5085b9338719a6ec1d04a830e9949b11d1c8d3
Brian Pandola <bpandola@gmail.com>
2026-04-17 01:13:13 -0700

    guardduty: migrate to automated parameter parsing

--- moto/guardduty/responses.py
+++ moto/guardduty/responses.py
@@ -1,26 +1,72 @@
 import json
-from urllib.parse import unquote
+from typing import Any, cast
 
-from moto.core.responses import BaseResponse
+from moto.core.parse import PROTOCOL_PARSERS
+from moto.core.responses import (
+    BaseResponse,
+    determine_request_protocol,
+    get_service_model,
+    normalize_request,
+)
 
 from .models import GuardDutyBackend, guardduty_backends
+
+# Mapping from URL regex group names to botocore serialization names
+_URI_PARAM_RENAME = {
+    "detector_id": "DetectorId",
+    "filter_name": "FilterName",
+}
 
 
 class GuardDutyResponse(BaseResponse):
     def __init__(self) -> None:
         super().__init__(service_name="guardduty")
+        self.automated_parameter_parsing = True
+
+    def parse_parameters(self, request: Any) -> None:
+        normalized_request = normalize_request(request)
+        service_model = get_service_model(self.boto3_service_name)
+        operation_model = service_model.operation_model(self._get_action())
+        protocol = determine_request_protocol(
+            service_model, normalized_request.content_type
+        )
+        parser_cls = PROTOCOL_PARSERS[protocol]
+        parser = parser_cls(operation_model, map_type=self.PROTOCOL_PARSER_MAP_TYPE)
+        # Rename URL regex group names to match botocore model expectations
+        url_params = {
+            _URI_PARAM_RENAME.get(k, k): v
+            for k, v in (self.uri_match.groupdict() if self.uri_match else {}).items()
+        }
+        parsed = parser.parse(
+            {
+                "method": normalized_request.method,
+                "values": normalized_request.values,
+                "headers": normalized_request.headers,
+                "body": normalized_request.data,
+                "url_path": normalized_request.path,
+                "url_params": url_params,
+            }
+        )
+        self.params = cast(Any, parsed)
 
     @property
     def guardduty_backend(self) -> GuardDutyBackend:
         return guardduty_backends[self.current_account][self.region]
 
+    def _get_body_param(self, param_name: str) -> Any:
+        """Get a parameter from the raw JSON body (camelCase) for values
+        stored as-is in models that use camelCase keys internally."""
+        if self.body:
+            return json.loads(self.body).get(param_name)
+        return None
+
     def create_filter(self) -> str:
-        detector_id = self.path.split("/")[-2]
-        name = self._get_param("name")
-        action = self._get_param("action")
-        description = self._get_param("description")
-        finding_criteria = self._get_param("findingCriteria")
-        rank = self._get_param("rank")
+        detector_id = self._get_param("DetectorId")
+        name = self._get_param("Name")
+        action = self._get_param("Action")
+        description = self._get_param("Description")
+        finding_criteria = self._get_body_param("findingCriteria")
+        rank = self._get_param("Rank")
 
         self.guardduty_backend.create_filter(
             detector_id, name, action, description, finding_criteria, rank
@@ -28,11 +74,11 @@ class GuardDutyResponse(BaseResponse):
         return json.dumps({"name": name})
 
     def create_detector(self) -> str:
-        enable = self._get_param("enable")
-        finding_publishing_frequency = self._get_param("findingPublishingFrequency")
-        data_sources = self._get_param("dataSources")
-        tags = self._get_param("tags")
-        features = self._get_param("features")
+        enable = self._get_param("Enable")
+        finding_publishing_frequency = self._get_param("FindingPublishingFrequency")
+        data_sources = self._get_body_param("dataSources")
+        tags = self._get_param("Tags")
+        features = self._get_body_param("features")
 
         detector_id = self.guardduty_backend.create_detector(
             enable, finding_publishing_frequency, data_sources, tags, features
@@ -41,20 +87,20 @@ class GuardDutyResponse(BaseResponse):
         return json.dumps({"detectorId": detector_id})
 
     def delete_detector(self) -> str:
-        detector_id = self.path.split("/")[-1]
+        detector_id = self._get_param("DetectorId")
 
         self.guardduty_backend.delete_detector(detector_id)
         return "{}"
 
     def delete_filter(self) -> str:
-        detector_id = self.path.split("/")[-3]
-        filter_name = unquote(self.path.split("/")[-1])
+        detector_id = self._get_param("DetectorId")
+        filter_name = self._get_param("FilterName")
 
         self.guardduty_backend.delete_filter(detector_id, filter_name)
         return "{}"
 
     def enable_organization_admin_account(self) -> str:
-        admin_account = self._get_param("adminAccountId")
+        admin_account = self._get_param("AdminAccountId")
         self.guardduty_backend.enable_organization_admin_account(admin_account)
 
         return "{}"
@@ -73,24 +119,24 @@ class GuardDutyResponse(BaseResponse):
         return json.dumps({"detectorIds": detector_ids})
 
     def get_detector(self) -> str:
-        detector_id = self.path.split("/")[-1]
+        detector_id = self._get_param("DetectorId")
 
         detector = self.guardduty_backend.get_detector(detector_id)
         return json.dumps(detector.to_json())
 
     def get_filter(self) -> str:
-        detector_id = self.path.split("/")[-3]
-        filter_name = unquote(self.path.split("/")[-1])
+        detector_id = self._get_param("DetectorId")
+        filter_name = self._get_param("FilterName")
 
         _filter = self.guardduty_backend.get_filter(detector_id, filter_name)
         return json.dumps(_filter.to_json())
 
     def update_detector(self) -> str:
-        detector_id = self.path.split("/")[-1]
-        enable = self._get_param("enable")
-        finding_publishing_frequency = self._get_param("findingPublishingFrequency")
-        data_sources = self._get_param("dataSources")
-        features = self._get_param("features")
+        detector_id = self._get_param("DetectorId")
+        enable = self._get_param("Enable")
+        finding_publishing_frequency = self._get_param("FindingPublishingFrequency")
+        data_sources = self._get_body_param("dataSources")
+        features = self._get_body_param("features")
 
         self.guardduty_backend.update_detector(
             detector_id, enable, finding_publishing_frequency, data_sources, features
@@ -98,12 +144,12 @@ class GuardDutyResponse(BaseResponse):
         return "{}"
 
     def update_filter(self) -> str:
-        detector_id = self.path.split("/")[-3]
-        filter_name = unquote(self.path.split("/")[-1])
-        action = self._get_param("action")
-        description = self._get_param("description")
-        finding_criteria = self._get_param("findingCriteria")
-        rank = self._get_param("rank")
+        detector_id = self._get_param("DetectorId")
+        filter_name = self._get_param("FilterName")
+        action = self._get_param("Action")
+        description = self._get_param("Description")
+        finding_criteria = self._get_body_param("findingCriteria")
+        rank = self._get_param("Rank")
 
         self.guardduty_backend.update_filter(
             detector_id,
@@ -117,6 +163,6 @@ class GuardDutyResponse(BaseResponse):
 
     def get_administrator_account(self) -> str:
         """Get administrator account details."""
-        detector_id = self.path.split("/")[-2]
+        detector_id = self._get_param("DetectorId")
         result = self.guardduty_backend.get_administrator_account(detector_id)
         return json.dumps(result)
