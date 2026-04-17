573ed4745e30b4bf75c0d6ce333eebffa8f754eb
Brian Pandola <bpandola@gmail.com>
2026-04-17 01:13:11 -0700

    dsql: migrate to automated parameter parsing

--- moto/dsql/responses.py
+++ moto/dsql/responses.py
@@ -1,6 +1,5 @@
 """Handles incoming dsql requests, invokes methods, returns responses."""
 
-import json
 from urllib.parse import unquote
 
 from moto.core.responses import ActionResult, BaseResponse
@@ -13,6 +12,7 @@ class AuroraDSQLResponse(BaseResponse):
 
     def __init__(self) -> None:
         super().__init__(service_name="dsql")
+        self.automated_parameter_parsing = True
 
     @property
     def dsql_backend(self) -> AuroraDSQLBackend:
@@ -20,7 +20,7 @@ class AuroraDSQLResponse(BaseResponse):
         return dsql_backends[self.current_account][self.region]
 
     def create_cluster(self) -> ActionResult:
-        params = json.loads(self.body)
+        params = self._get_params()
         deletion_protection_enabled = params.get("deletionProtectionEnabled", True)
         tags = params.get("tags")
         client_token = params.get("clientToken")
@@ -32,7 +32,7 @@ class AuroraDSQLResponse(BaseResponse):
         return ActionResult(cluster)
 
     def delete_cluster(self) -> ActionResult:
-        identifier = self.path.split("/")[-1]
+        identifier = self._get_param("identifier")
         cluster = self.dsql_backend.delete_cluster(identifier=identifier)
         result = {
             "identifier": cluster.identifier,
@@ -43,17 +43,17 @@ class AuroraDSQLResponse(BaseResponse):
         return ActionResult(result)
 
     def get_cluster(self) -> ActionResult:
-        identifier = self.path.split("/")[-1]
+        identifier = self._get_param("identifier")
         cluster = self.dsql_backend.get_cluster(identifier=identifier)
         return ActionResult(cluster)
 
     def get_vpc_endpoint_service_name(self) -> ActionResult:
-        identifier = self.path.split("/")[-2]
+        identifier = self._get_param("identifier")
         result = self.dsql_backend.get_vpc_endpoint_service_name(identifier)
         return ActionResult(result)
 
     def list_tags_for_resource(self) -> ActionResult:
-        arn = unquote(self.path.split("/")[-1])
-        identifier = arn.split("/")[-1]
+        resource_arn = unquote(self._get_param("resourceArn"))
+        identifier = resource_arn.split("/")[-1]
         tags = self.dsql_backend.list_tags_for_resource(identifier)
         return ActionResult({"tags": tags})
